f655b2546c8266ce22bbde93e6b72273d95609a9
yevini118 <79689350+yevini118@users.noreply.github.com>
2022-03-20 16:52:52 +0900

    Create 베스트앨범.py

---
+++ level3/베스트앨범.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from collections import Counter;
+
+def solution(genres, plays):
+    
+    
+    music_dict = {i: [genres[i], plays[i]] for i in range(0,len(plays))}
+    genre_sum = {genre : 0 for genre in set(genres)}
+    answer = []
+
+    for music in music_dict.values():
+        genre_sum[music[0]] += music[1] 
+
+    genre_sum = dict(sorted(genre_sum.items(), key= lambda k: -k[1]))
+    music_dict= dict(sorted(music_dict.items(), key= lambda k: (-k[1][1], k[0])))
+
+    for genre in genre_sum:
+        count = 0
+        for id, music in music_dict.items():
+            if count == 2:
+                break
+            elif genre == music[0]:
+                answer.append(id)
+                count += 1
+
+    return answer
+
+
+
+ 
+
+
