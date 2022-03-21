c10be4d6c01adf29b7fef2ff3d145186d2f0c19b
yevini118 <79689350+yevini118@users.noreply.github.com>
2022-03-22 05:03:11 +0900

    Update 기능개발.py

--- level2/기능개발.py
+++ level2/기능개발.py
@@ -3,14 +3,14 @@ import math
 def solution(progresses, speeds):
     
     answer = []
-    days = [math.ceil((100 - progresses[i])/speeds[i]) for i in range(len(progresses))]
+    days = [math.ceil((100 - progresses[i])/speeds[i]) for i in range(len(progresses))] #개발일수
     days.reverse()
 
     while(days):
 
-        top = days.pop()
+        top = days.pop() #앞에있는 기능
         count = 1
-        while(days and top >= days[-1]):
+        while(days and top >= days[-1]): #뒤에있는 기능중 개발 완료된 기능
             days.pop()
             count += 1
 
